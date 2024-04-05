9bbbde134ee745bd31ebc07ccce15f45a87104e6
zhiliao <1261317794@qq.com>
2024-04-05 20:58:37 +0800

    fitst commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import home from './pages/home/index.vue'
+
+</script>
+
+<template>
+  <header>
+    <home></home>
+
+  </header>
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped></style>
